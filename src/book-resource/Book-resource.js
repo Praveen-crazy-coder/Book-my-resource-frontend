5d91516b61844a6feec9c597cab745f333950801
abine-praveen <praveen.kumar@joindeleteme.com>
2024-09-29 12:05:02 +0530

    fix ci/cd

--- src/book-resource/Book-resource.js
+++ src/book-resource/Book-resource.js
@@ -132,11 +132,11 @@ function BookResource() {
                     {/* Pagination */}
                     <nav aria-label="Page navigation example" style={{ marginLeft: "20px" }}>
                         <ul className="pagination">
-                            <li className="page-item"><a className="page-link" href"./">Previous</a></li>
-                            <li className="page-item"><a className="page-link" href"./">1</a></li>
-                            <li className="page-item"><a className="page-link" href"./">2</a></li>
-                            <li className="page-item"><a className="page-link" href"./">3</a></li>
-                            <li className="page-item"><a className="page-link" href"./">Next</a></li>
+                            <li className="page-item"><a className="page-link" href"/">Previous</a></li>
+                            <li className="page-item"><a className="page-link" href"/">1</a></li>
+                            <li className="page-item"><a className="page-link" href"/">2</a></li>
+                            <li className="page-item"><a className="page-link" href"/">3</a></li>
+                            <li className="page-item"><a className="page-link" href"/">Next</a></li>
                         </ul>
                     </nav>
                 </div>
